8aa8805c24bd8da2d3b31fea5481132e99fe1250
Jodel Labios <jodellabios866@gmail.com>
2026-05-21 00:06:45 +0800

    library card
    libra

--- research-archive/src/index.js
+++ research-archive/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import LibraryCard from './LibraryCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LibraryCard />
   </React.StrictMode>
 );
 
